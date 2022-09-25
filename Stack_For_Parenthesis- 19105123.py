66cfe1ddfaf9b86aaf03eef761d6927579d7d27b
Annchit <63941503+Annchit@users.noreply.github.com>
2022-09-25 20:33:14 +0530

    Update Stack_For_Parenthesis- 19105123.py

--- Stack_For_Parenthesis- 19105123.py
+++ Stack_For_Parenthesis- 19105123.py
@@ -7,9 +7,13 @@ def isBalanced(parentheses):
         if char in ["(", "{", "["]:
             utilStack.append(char)
         
+        # if the character in input is an open bracket, then we push it into the stack
+        
         else:
             if not utilStack:
                 return False
+            # if stack is already empty then no pop can occur, so false
+            
             current_char = utilStack.pop()
             if current_char == '(':
                 if char != ")":
@@ -20,9 +24,11 @@ def isBalanced(parentheses):
             if current_char == '[':
                 if char != "]":
                     return False
-
+            # if the current character is not the closing bracket for the one popped from the stack, then again it is unbalanced
+            
     if utilStack:
         return False
+    # if at the end of all input, the stack is not empty, then the input was unbalanced
 
     return True
 
