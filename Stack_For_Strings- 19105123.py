2fb1d415f130ad4f016bc81329250069fce8dfdd
Annchit <63941503+Annchit@users.noreply.github.com>
2022-09-25 20:41:34 +0530

    Update Stack_For_Strings- 19105123.py

--- Stack_For_Strings- 19105123.py
+++ Stack_For_Strings- 19105123.py
@@ -6,14 +6,20 @@ def isEmpty(tempStack):
         return True
     else:
         return False
+    
+# isEmpty - checks if the stack is empty or not
 
 def getSize(tempStack):
     return len(tempStack)
+
+# getSize - returns the size of the stack
     
 def push(tempStack, entity):
     global top
     tempStack.append(entity)
     top=len(tempStack)-1
+    
+# push - pushes another character into the stack
     
 def pop(tempStack):
     global top
@@ -27,12 +33,16 @@ def pop(tempStack):
             top=top-1
     return i
 
+# pop - takes out the topmost element from the stack and if the stack is already empty then informs about the underflow
+
 def peek(tempStack):
     if(isEmpty(tempStack)):
         return('underflow')
     else:
         top=len(tempStack)-1
         return tempStack[top]
+    
+# peek - returns the topmost element of the stack
 
 def display(tempStack):
     if(isEmpty(tempStack)):
@@ -44,6 +54,8 @@ def display(tempStack):
         for i in range(top-1,-1,-1):
             print(tempStack[i])
             
+# display - prints all the elements of the stack
+            
 while True:
     print('Implementation of Stack')
     print('1. Push')
@@ -54,21 +66,20 @@ while True:
     print('6. Exit')
     
     choice=int(input('Enter a choice (1-6)'))
+    
     if(choice==1):
         entity=(input('Enter the entity that you want to push:'))
         push(mainStack,entity)
         print('\n')
         print('%s added successfully...' %entity)
-        
-        
+    
     elif(choice==2):
         entity =pop(mainStack)
         if (entity=='underflow'):
             print('stack already empty...')
         else:
             print('\n')
-            print('%s is popped' %entity)
-             
+            print('%s is popped' %entity)            
             
     elif(choice==3):
         entity =peek(mainStack)
@@ -77,19 +88,16 @@ while True:
         else:
             print('\n')
             print('%s is at the top' %entity)
-             
-            
+     
     elif(choice==4):
         print('\n')
         display(mainStack)
-        
         
     elif(choice==5):
         entity = getSize(mainStack)
         print('\n')
         print('%s is the size of the stack ' %entity)
-        
-
+   
     elif(choice==6):
         break
         
